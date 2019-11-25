d8b3cc726ea420e2e967f2fadacf63ef61cc67ad
prisar <pritimaysarkar2010@gmail.com>
2019-11-25 22:51:38 +0530

    doubts

---
+++ src/models/user.model.js
@@ -0,0 +1,8 @@
+const { map, pick } = require('lodash'); // optimized
+// const _ = require('lodash');
+
+map(arr)
+
+var employees = [];
+
+var happy_employees = _.map(employees, x => x.payout + 10000);
