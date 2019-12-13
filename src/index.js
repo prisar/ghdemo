7a77e9507762fe81faba059c1d52f4d613f0456f
prisar <pritimaysarkar2010@gmail.com>
2019-12-13 23:44:14 +0530

    nodemon

--- src/index.js
+++ src/index.js
@@ -1,53 +1,11 @@
-import { ok } from "assert";
-import { userInfo } from "os";
+const app = require('express');
 
 
-let arr = [1, 2, 3];
+// listen to requests
+app.listen(8080, () => console.info(`server started on port ${port} (${env})`));
 
-arr = arr.forEach(x => {
-    x = x + 2;
-    console.log(x);
-});
-
-// ...arr
-
-const user = {
-    username: 'ram',
-    firstname: 'ram',
-    lastname: 'babu',
-};
-
-// destructing
-const { username } = user;
-
-
-// console.log(arr);
-
-// IIFE - immidiately invoked function Exec
-
-(function (a, b) {
-    return a+b;
-
-})(1, 2);
-
-(function () {
-    return a+b;
-
-})();
-
-200 -> ok
-404 -> 
-403 -> UnAuthorized, 
-500 -> Internal Server Error
-
-
-301 -> 
-try {
-    //djdjd
-
-} catch (ex) {
-    APIError(ex.message);
-}
-
-ES6
-
+/**
+* Exports express
+* @public
+*/
+module.exports = app;
